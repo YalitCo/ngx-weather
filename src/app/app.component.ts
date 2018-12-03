65b6aa0c32759c1585d38d02241f2e647596932d
mersad <mersad.hunter@gmail.com>
2018-12-03 17:09:27 +0330

    handle wronge or emty input(city)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,41 +17,48 @@ export class AppComponent {
   url: string;
 
   citys: ICitys[] = [];
- 
+
   citysArray;
+
+  show: boolean = false;
 
   constructor(private apiService: ApiService) { }
 
 
   OnInputChange(event: any) {
-    console.log(event);
+    //console.log(event);
     this.value = (<HTMLInputElement>event.target).value;
   }
 
   OnSearchClick() {
-    this.url = "https://api.openweathermap.org/data/2.5/weather?q=" + this.value + "&units=metric&APPID=7ea8f559ec5f1f0899b61c89d728a8b9";
-    
-    this.apiService
-      .getPosts(this.url)
-      .subscribe(
-        //response => console.log(response)
-        response => {
-          
-          this.citysArray = response;
-          let c: ICitys = {
-            name: this.citysArray.name,
-            desc: this.citysArray.weather[0].description ,
-            icon: "assets/pics/"+this.citysArray.weather[0].icon+".png",
-            temp: this.citysArray.main.temp
-          }
-          this.citys.push(c);
-        }
-         
-      );
-     //console.log(this.citysArray);
-    
-  }
+    if (this.value != '') {
+      this.url = "https://api.openweathermap.org/data/2.5/weather?q=" + this.value + "&units=metric&APPID=7ea8f559ec5f1f0899b61c89d728a8b9";
 
+      this.apiService
+        .getPosts(this.url)
+        .subscribe(
+          //response => console.log(response)
+          response => {
+
+            this.citysArray = response;
+            let c: ICitys = {
+              name: this.citysArray.name,
+              desc: this.citysArray.weather[0].description,
+              icon: "assets/pics/" + this.citysArray.weather[0].icon + ".png",
+              temp: Math.round(this.citysArray.main.temp)
+            }
+            this.citys.push(c);
+          },
+          error => {
+            this.show = true;
+          }
+
+        );
+    }
+    else{
+      this.show = true;
+    }
+  }
 
 }
 
--- src/app/weather-items/weather-items.component.ts
+++ src/app/weather-items/weather-items.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ValueConverter } from '@angular/compiler/src/render3/view/template';
-import { ApiService } from "../api.service";
-import { ICitys } from '../icitys';
-import { CompileStylesheetMetadata } from '@angular/compiler';
 
 @Component({
   selector: 'app-weather-items',
@@ -12,22 +8,10 @@ import { CompileStylesheetMetadata } from '@angular/compiler';
 
 export class WeatherItemsComponent implements OnInit {
 
-
-  // url = "https://api.openweathermap.org/data/2.5/weather?q="+this.city+"&units=metric&APPID=7ea8f559ec5f1f0899b61c89d728a8b9";
-  //@Input() citys: string='mashhad';
-
 @Input() c:string;
   
-  
-  // constructor(private apiService: ApiService) {}
-
   ngOnInit() {
-    // this.apiService
-    //   .getPosts(this.url)
-    //   .subscribe(
-    //     response => console.log(response)
-    //    // response => this.postsArray = response
-    //   );
+
   }
 
 
